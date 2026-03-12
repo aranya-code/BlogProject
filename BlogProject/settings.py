52a5a48e85900fb2e5cbe91fb7b12c2904d93330
Aranya Majumdar <74165561+aranya-code@users.noreply.github.com>
2026-03-12 21:44:33 +0530

    add requirements

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -19,7 +19,7 @@ load_dotenv(BASE_DIR / ".env")
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "").lower() == "true"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ["aranya.pythonanywhere.com"]
 
 
 # Quick-start development settings - unsuitable for production
@@ -114,9 +114,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
 
-STATICDIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -125,3 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = BASE_DIR / "uploads"
 MEDIA_URL = "/media/"
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://aranya.pythonanywhere.com"
+]
