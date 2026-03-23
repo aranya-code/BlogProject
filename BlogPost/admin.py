ac63f076687f0991aec8c23f0edc68ec7d59392d
Aranya Majumdar <74165561+aranya-code@users.noreply.github.com>
2026-03-23 15:17:48 +0530

    Customize admin panel

--- BlogPost/admin.py
+++ BlogPost/admin.py
@@ -1,9 +1,50 @@
 from django.contrib import admin
 from .models import Author, Post, Comment
+from django.utils.html import format_html
 
-# Register your models here.
+# Inline Comments
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+    readonly_fields =('date',)
 
-admin.site.register(Author)
-admin.site.register(Post)
-admin.site.register(Comment)
+# Customizing Author Admin view
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display= ('full_name', 'email_address')
+    search_fields= ('full_name',)
+
+
+# Customizing Post Admin view
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # list_display = ('title', 'author', 'date')
+    list_select_related = ('author',)
+    search_fields = ('title', 'author')
+    list_filter = ('author',)
+    ordering = ('-date',) # Latest post first
+    readonly_fields = ('date',)
+
+    fieldsets = (
+        ('Post Info', {
+            'fields': ('title', 'author')
+        }),
+        ('MetaData',{
+            'fields': ('slug', 'content'),
+            'classes': ('collapse',)
+        })
+    )
+
+    # Creating a preview of article
+    def short_content(self, obj):
+        return obj.content[:50]    
+    short_content.short_description = 'Preview'
+    list_display = ('title', 'short_content', 'author')
+
+    # Inclusion of images
+    def image_tag(self, obj):
+        return format_html('<img src="{}" width="50"/>', obj.image.url)
+    image_tag.short_description= "Image"
+    list_display = ('title', 'short_content', 'image_tag', 'author')
+
 
--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = os.environ.get("DEBUG", "").lower() == "True"
+DEBUG = os.environ.get("DEBUG", "").lower() == "true"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS",'').split(',')
 
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'BlogPost',
+    
 ]
 
 MIDDLEWARE = [
